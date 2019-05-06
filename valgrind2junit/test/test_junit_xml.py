ce3d1ee4787204c54e814a366c892e5121e9f82d
Joseph Luce <luceCoding@gmail.com>
2019-05-06 02:21:08 -0700

    Initial commit of tests for valgrind errors and parsers.

---
+++ valgrind2junit/test/test_junit_xml.py
@@ -0,0 +1,10 @@
+import unittest
+
+class test_junit_xml(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_
---
+++ valgrind2junit/test/test_valgrind_parser.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+from parser.valgrind_parser import ValgrindParser
+import pytest
+
+class test_valgrind_parser(unittest.TestCase):
+    def setUp(self):
+        self.valgrind_test_file = os.path.join(os.path.dirname(__file__), 'valgrind-test.xml')
+
+    def tearDown(self):
+        pass
+
+    def test_get_next_error(self):
+        parser = ValgrindParser(self.valgrind_test_file)
+        error_generator = parser.get_next_error()
+        count = 0
+        for error in error_generator:
+            count += 1
+            assert error is not None
+        assert count == 2
+        with pytest.raises(Exception):
+            next(error_generator)
